94d3c84acb0f4a8b24ea81d7dcab0f69ec45136c
Shiro <435419123@qq.com>
2018-05-28 18:16:15 +0800

    从IDEA提交，并且修改了README.MD

--- src/com/test/Test01.java
+++ src/com/test/Test01.java
@@ -1,11 +1,14 @@
 package com.test;
 
+import java.util.Scanner;
+
 /**
  *
  */
 public class Test01 {
     public static void main(String[] args) {
+        Integer inte = new Integer("3");
         System.out.println("hello Idea");
-
+        System.out.println(inte);
     }
 }
